fa27d9fbbb62f384012c1b148fef05131ffcc0cf
GamerZone1006 <anushkayadav1006@gmail.com>
2025-04-29 19:06:30 +0530

    day4 q5

--- day4.java
+++ day4.java
@@ -23,17 +23,27 @@ public class day4 {
     // return result;
     // }
 
-    public static int[] getConcatenation(int[] nums) {
-        int n1 = nums.length;
-        int[] num2 = new int[n1];
-        for (int i = 0; i < n1; i++) {
-            num2[i] = nums[i];
+    // public static int[] getConcatenation(int[] nums) {
+    //     int n1 = nums.length;
+    //     int[] num2 = new int[n1];
+    //     for (int i = 0; i < n1; i++) {
+    //         num2[i] = nums[i];
+    //     }
+    //     int n2 = num2.length;
+    //     int[] ans = new int[n1 + n2];
+    //     System.arraycopy(nums, 0, ans, 0, n1);
+    //     System.arraycopy(num2, 0, ans, n1, n2);
+    //     return ans;
+    // }
+
+    public static int[] runningSum(int[] arr){
+        int sum = 0;
+        int[] result = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            result[i] = sum;
         }
-        int n2 = num2.length;
-        int[] ans = new int[n1 + n2];
-        System.arraycopy(nums, 0, ans, 0, n1);
-        System.arraycopy(num2, 0, ans, n1, n2);
-        return ans;
+        return result;
     }
 
     public static void main(String[] args) {
@@ -52,9 +62,13 @@ public class day4 {
         // int[] result = concat(arr1, arr2);
         // System.out.println("Concatenated string: "+Arrays.toString(result));
 
-        int[] arr = { 1, 2, 1 };
-        int[] ans = getConcatenation(arr);
-        System.out.println(Arrays.toString(ans));
+        // int[] arr = { 1, 2, 1 };
+        // int[] ans = getConcatenation(arr);
+        // System.out.println(Arrays.toString(ans));
+
+        int[] arr = {1,2,3,4};
+        int[] res = runningSum(arr);
+        System.out.println(Arrays.toString(res));
 
         sc.close();
     }
