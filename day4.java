2460b61ecb86df69b93571d8cbd30245e3f0906a
GamerZone1006 <anushkayadav1006@gmail.com>
2025-04-29 11:20:34 +0530

    day4 q1

---
+++ day4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class day4 {
+    // public static boolean age(int age){
+    //     if(age<18){
+    //         return false;
+    //     }
+    //     else if (age>=18) {
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter age: ");
+        // int a = sc.nextInt();
+        // if (age(a)) {
+        //     System.out.println("Eligible");
+        // }
+        // else{
+        //     System.out.println("Not eligible.");
+        // }
+
+        sc.close();   
+    }
+}
