18af47bade0e1235195095d521fae2f571c5efa0
GamerZone1006 <anushkayadav1006@gmail.com>
2025-04-29 18:18:27 +0530

    day4 q2

--- day4.java
+++ day4.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class day4 {
     // public static boolean age(int age){
@@ -11,6 +12,15 @@ public class day4 {
     //         return false;
     //     }
     // }
+
+    public static int[] concat(int[] arr1, int[] arr2){
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int[] result = new int[n1+n2];
+        System.arraycopy(arr1, 0, result, 0, n1);
+        System.arraycopy(arr2, 0, result, n1, n2);
+        return result;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.print("Enter age: ");
@@ -22,6 +32,10 @@ public class day4 {
         //     System.out.println("Not eligible.");
         // }
 
+        int[] arr1 = {1,2,3,4};
+        int[] arr2 = {5,6,7,8};
+        int[] result = concat(arr1, arr2);
+        System.out.println("Concatenated string: "+Arrays.toString(result));
         sc.close();   
     }
 }
