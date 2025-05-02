86d450203bd017b31ba7b214da00d129df05b683
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-02 23:49:52 +0530

    day6 q2

--- day6.java
+++ day6.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
+
 public class day6 {
-    public static int identical(int[] arr){
-        int sum = 0;
+    // public static int identical(int[] arr){
+    //     int sum = 0;
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=0;j<i;j++){
+    //             if (arr[i] == arr[j]) {
+    //                 sum += 1;
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // }
+
+    public static ArrayList<Integer> less(int[] arr){
+        // int[] res = new int[arr.length];
+        ArrayList<Integer> res = new ArrayList<>();
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<i;j++){
-                if (arr[i] == arr[j]) {
+            int sum = 0;
+            for(int j=0;j<arr.length;j++){
+                if (arr[j] < arr[i]) {
                     sum += 1;
                 }
             }
+            res.add(sum);
         }
-        return sum;
+        return res;
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,1,1,3};
-        System.out.println("Identical pairs: "+identical(arr));
+        // int[] arr = {1,2,3,1,1,3};
+        // System.out.println("Identical pairs: "+identical(arr));
+
+        int[] arr = {8,1,2,2,3};
+        System.out.println(less(arr));
     }
 }
