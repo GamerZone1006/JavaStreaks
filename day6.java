30d1d0e7e9fb276f1159872c822b4d4bdf1c069e
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-03 01:18:11 +0530

    day7 q1

--- day6.java
+++ day6.java
@@ -13,26 +13,29 @@ public class day6 {
     //     return sum;
     // }
 
-    public static ArrayList<Integer> less(int[] arr){
-        // int[] res = new int[arr.length];
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            int sum = 0;
-            for(int j=0;j<arr.length;j++){
-                if (arr[j] < arr[i]) {
-                    sum += 1;
-                }
-            }
-            res.add(sum);
-        }
-        return res;
-    }
+    // public static ArrayList<Integer> less(int[] arr){
+    //     // int[] res = new int[arr.length];
+    //     ArrayList<Integer> res = new ArrayList<>();
+    //     for(int i=0;i<arr.length;i++){
+    //         int sum = 0;
+    //         for(int j=0;j<arr.length;j++){
+    //             if (arr[j] < arr[i]) {
+    //                 sum += 1;
+    //             }
+    //         }
+    //         res.add(sum);
+    //     }
+    //     return res;
+    // }.
+
+
 
     public static void main(String[] args) {
         // int[] arr = {1,2,3,1,1,3};
         // System.out.println("Identical pairs: "+identical(arr));
 
-        int[] arr = {8,1,2,2,3};
-        System.out.println(less(arr));
+        // int[] arr = {8,1,2,2,3};
+        // System.out.println(less(arr));
+
     }
 }
---
+++ day7.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class day7 {
+    public static ArrayList<Integer> indexmatch(int[] nums, int[] index){
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            res.add(nums[i],index[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        System.out.println(indexmatch(nums, index));
+    }
+}
