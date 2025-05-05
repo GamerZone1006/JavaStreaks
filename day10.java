94b719b4d66a1f59afe456edd2ff05acdc371c1f
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-06 03:54:31 +0530

    q3

--- day10.java
+++ day10.java
@@ -14,15 +14,15 @@ public class day10 {
     //     }
     // }
 
-    public static int maxIndex(int[] arr,int last){
-        int max = 0;
-        for(int i=0;i<=last;i++){
-            if (arr[i] > arr[max]) {
-                max = i;
-            }
-        }
-        return max;
-    }
+    // public static int maxIndex(int[] arr,int last){
+    //     int max = 0;
+    //     for(int i=0;i<=last;i++){
+    //         if (arr[i] > arr[max]) {
+    //             max = i;
+    //         }
+    //     }
+    //     return max;
+    // }
 
     public static void swap(int[] arr, int first, int second){
         int temp = arr[first];
@@ -30,20 +30,35 @@ public class day10 {
         arr[second] = temp;
     }
 
-    public static void selectionSort(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            int last = arr.length - i - 1;
-            int maxIn = maxIndex(arr, last);
-            swap(arr, maxIn, last);
+    // public static void selectionSort(int[] arr){
+    //     for(int i=0;i<arr.length;i++){
+    //         int last = arr.length - i - 1;
+    //         int maxIn = maxIndex(arr, last);
+    //         swap(arr, maxIn, last);
+    //     }
+    // }   
+    
+    public static void insertionSort(int[] arr){
+        for(int i=0;i<arr.length - 1;i++){
+            for(int j=i+1;j>0;j--){
+                if (arr[j]<arr[j-1]) {
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
+                }
+            }
         }
-    }    
+    }
 
     public static void main(String[] args) {
         int[] arr = {3,1,2,5,4};
         // bubbleSort(arr);
-        // System.out.println(Arrays.toString(arr));
 
-        selectionSort(arr);
+        // selectionSort(arr);
+
+        insertionSort(arr);
+
         System.out.println(Arrays.toString(arr));
     }
 }
