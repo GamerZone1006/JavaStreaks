ae49c9432be3f897caf618d19ff0d592ebc9d5ee
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-07 02:20:28 +0530

    missing number

--- day10.java
+++ day10.java
@@ -51,23 +51,43 @@ public class day10 {
     //     }
     // }
 
-    public static void cyclicSort(int[] arr){
-        int i=0;
+    // public static void cyclicSort(int[] arr){
+    //     int i=0;
+    //     while (i<arr.length) {
+    //         int index = arr[i] - 1;
+    //         if (arr[i] != arr[index]) {
+    //             swap(arr, i, index);
+    //         }
+    //         else{i++;}
+    //     }
+    // }
+
+    public static int missingNo(int[] arr){
+        int i = 0;
         while (i<arr.length) {
-            int index = arr[i] - 1;
-            if (arr[i] != arr[index]) {
-                swap(arr, i, index);
+            int correctInd = arr[i];
+            if (arr[i]<arr.length && arr[i] != arr[correctInd]) {
+                swap(arr, i, correctInd);
             }
             else{i++;}
         }
+        for(int j=0;j<arr.length;j++){
+            if (arr[j] != j) {
+                return j;
+            }
+        }
+        return arr.length;
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,1,2,5,4};
+        // int[] arr = {3,1,2,5,4};
         // bubbleSort(arr);
         // selectionSort(arr);
         // insertionSort(arr);
-        cyclicSort(arr);
-        System.out.println(Arrays.toString(arr));
+        // cyclicSort(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        int[] arr = {4,0,1,2};
+        System.out.println(missingNo(arr));
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,6 @@
-arr
+for( i=0;i<arr.length;i++){
+                
+                if (arr[i] != correctIndex) {
+                    missing = arr[i];
+                }
+            }
