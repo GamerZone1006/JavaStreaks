74f9d8829e552a9c6539c92fd13d32f9b7ee5d8f
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-26 23:21:25 +0530

    Selection sort question

--- bubblesort.java
+++ bubblesort.java
@@ -1,27 +1,41 @@
 import java.util.Arrays;
 
 public class bubblesort {
-    public static void bubbleSort(int[] arr){
-        boolean swapped;
-        for(int i=0;i<arr.length;i++){
-            swapped = false;
-            for(int j=1;j<arr.length - i;j++){
-                if (arr[j]<arr[j-1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-    }
-    public static void main(String[] args) {
-        int[] arr = {3,2,1,5,4};
-        bubbleSort(arr);
+    // public static void bubbleSort(int[] arr){
+    //     boolean swapped;
+    //     for(int i=0;i<arr.length;i++){
+    //         swapped = false;
+    //         for(int j=1;j<arr.length - i;j++){
+    //             if (arr[j]<arr[j-1]) {
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j-1];
+    //                 arr[j-1] = temp;
+    //                 swapped = true;
+    //             }
+    //         }
+    //         if (!swapped) {
+    //             break;
+    //         }
+    //     }
+    //     System.out.println(Arrays.toString(arr));
+    // }
 
+    // public static void q1(int[] arr){
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=1;j<arr.length - i;j++){
+    //             if (arr[j]<arr[j-1]) {
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j-1];
+    //                 arr[j-1] = temp;
+    //             }
+    //         }
+    //     }
+    //     System.out.println(Arrays.toString(arr));
+    // }
+    public static void main(String[] args) {
+        // int[] arr = {3,2,1,5,4};
+        // bubbleSort(arr);
+        // int[] arr = {5, 2, 9, 1, 5, 6};
+        // q1(arr);
     }
 }
---
+++ selectionsort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class selectionsort {
+    // public static int max(int[] arr, int start, int end){
+    // int max = start;
+    // for(int i=start;i<end;i++){
+    // if (arr[max] < arr[i]) {
+    // max = i;
+    // }
+    // }
+    // return max;
+    // }
+
+    // public static void swap(int[] arr, int first, int last){
+    // for(int i=0;i<arr.length;i++){
+    // int temp = arr[first];
+    // arr[first] = arr[last];
+    // arr[last] = temp;
+    // }
+    // }
+
+    // public static void ss(int[] arr){
+    // for(int i=0;i<arr.length;i++){
+    // int last = arr.length -i - 1;
+    // int maxInd = max(arr, 0, last);
+    // swap(arr, maxInd, last);
+    // }
+    // System.out.println(Arrays.toString(arr));
+    // }
+
+    // Sorting in descending order
+    public static int getMin(int[] arr, int start, int end) {
+        int min = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[min] > arr[i]) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    public static void swap(int[] arr, int first, int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+
+    public static void desc(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i -1;
+            int minInd = getMin(arr, 0, last);
+            swap(arr, minInd, last);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = {3,4,2,5,1};
+        // ss(arr);
+        int[] arr = { 3, 10, 4, 2 };
+        desc(arr);
+    }
+}
