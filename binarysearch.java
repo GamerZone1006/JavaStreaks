10d267fbae8b83582b474bf4eaf96593b5819812
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-26 19:17:45 +0530

    Binary Search

--- binarysearch.java
+++ binarysearch.java
@@ -1,21 +1,23 @@
+import java.util.ArrayList;
+
 public class binarysearch {
-    // public static int bs(int[] arr, int target){
-    //     int start = 0;
-    //     int end = arr.length - 1;
-    //     while(start<=end){
-    //         int mid = (end - start)/2 + start;
-    //         if (arr[mid] == target) {
-    //             return mid;
-    //         }
-    //         else if (target< arr[mid]) {
-    //             end = mid - 1;
-    //         }
-    //         else{
-    //             start = mid + 1;
-    //         }
-    //     }
-    //     return -1;
-    // }
+    public static int bs(int[] arr, int target, int start, int end){
+        start = 0;
+        end = arr.length - 1;
+        while(start<=end){
+            int mid = (end - start)/2 + start;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (target< arr[mid]) {
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 
     // public static boolean isAesc(int[] arr){
     //     int start = 0;
@@ -61,27 +63,94 @@ public class binarysearch {
         // for floor of element return arr[end];
     // }
 
-    public static char ceil(char[] letters, char target){
+    // public static char ceil(char[] letters, char target){
+    //     int start = 0;
+    //     int end = letters.length - 1;
+    //     while (start<=end) {
+    //         int mid = (end - start)/2 + start;
+    //         if(target<letters[mid]) end = mid - 1;
+    //         else start = mid + 1;
+    //     }
+    //     return letters[start % letters.length];
+    // }
+
+    // public static ArrayList<Integer> aagePeeche(int[] arr, int target){
+    //     int start = 0;
+    //     int end = arr.length - 1;
+    //     ArrayList<Integer> res = new ArrayList<>();
+    //     while (start<= end) {
+    //         int mid = (end - start)/2 + start;
+    //         int peeche = arr.length - mid;
+    //         if (arr[mid] == target) {
+    //             res.add(mid);
+    //             res.add(peeche);
+    //         }
+    //         if(target<arr[mid]) end = mid - 1;
+    //         else start = mid + 1;
+    //     }
+    //     return res;
+    // }
+
+    // public static ArrayList<Integer> occur(int[] arr, int target){
+    //     int start = 0;
+    //     int end = arr.length - 1;
+    //     int first = -1;
+    //     int last = -1;
+    //     ArrayList<Integer> res = new ArrayList<>();
+    //     while (start<=end) {
+    //         int mid = (end - start)/2 + start;
+    //         if (target == arr[mid]) {
+    //             first = mid;
+    //             end = mid - 1;
+    //         }
+    //         else if(target<arr[mid]) end = mid - 1;
+    //         else start = mid + 1;
+    //     }
+    //     start = 0;
+    //     end = arr.length - 1;
+    //     while (start<=end) {
+    //         int mid = (end - start)/2 + start;
+    //         if (target == arr[mid]) {
+    //             last = mid;
+    //             start = mid + 1;
+    //         }
+    //         else if(target<arr[mid]) end = mid - 1;
+    //         else start = mid + 1;
+    //     }
+    //     res.add(first);
+    //     res.add(last);
+    //     return res;
+    // }
+
+    public static int infArray(int[] arr, int target){
         int start = 0;
-        int end = letters.length - 1;
-        while (start<=end) {
-            int mid = (end - start)/2 + start;
-            if(target<letters[mid]) end = mid - 1;
-            else start = mid + 1;
+        int end = 1;
+        while (target<arr[end]) {
+            int newStart = end + 1;
+            end = end + (end - start + 1)*2;
+            start = newStart;
         }
-        return letters[start % letters.length];
+        return bs(arr, target, start, end);
     }
 
     public static void main(String[] args) {
-        // int[] arr = {2,3, 13, 17, 20, 23, 34, 55, 78};
+        int[] arr = {2,3, 13, 17, 20, 23, 34, 55, 78};
         // int target = 16;
         // int res = bs(arr, target);
         // int res = oabs(arr, target);
         // int res = ceiling(arr, target);
         // System.out.println(res);
-        char[] letter = {'c','f','j'};
-        char target = 'a';
-        char res = ceil(letter, target);
+        // char[] letter = {'c','f','j'};
+        // char target = 'a';
+        // char res = ceil(letter, target);
+        // System.out.println(res);
+        // int target = 17;
+        // ArrayList<Integer> res = aagePeeche(arr, target);
+        // int[] arr = {1, 1, 1, 2, 2, 2, 2, 2};
+        // int target = 2;
+        // ArrayList<Integer> res = occur(arr, target);
+        int target = 55;
+        int res = infArray(arr, target);
         System.out.println(res);
     }
 }
