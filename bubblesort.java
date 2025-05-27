e25ad149b95e9bfb02e96f5f7135df11ddb21c4e
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-27 12:59:42 +0530

    String

--- bubblesort.java
+++ bubblesort.java
@@ -35,6 +35,44 @@ public class bubblesort {
     // }
 
     // removing duplicate elements from sorted array
+    // public static void bs(int[] arr){
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=1;j<arr.length - i;j++){
+    //             if (arr[j]<arr[j-1]) {
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j-1];
+    //                 arr[j-1] = temp;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // public static void duplicate(int[] arr){
+    //     bs(arr);
+    //     ArrayList<Integer> res = new ArrayList<>();
+    //     for(int i=0;i<arr.length - 1;i++){
+    //         if (arr[i] != arr[i+1]) {
+    //             res.add(arr[i]);
+    //         }
+    //     }
+    //     res.add(arr[arr.length - 1]);
+    //     System.out.println(res);
+    // }
+
+    public static int[] strLenArr(String[] arr){
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            int len = arr[i].length();
+            res.add(len);
+        }
+        int[] array = new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            array[i] = res.get(i);
+        }
+        bs(array);
+        return array;
+    }
+
     public static void bs(int[] arr){
         for(int i=0;i<arr.length;i++){
             for(int j=1;j<arr.length - i;j++){
@@ -45,18 +83,7 @@ public class bubblesort {
                 }
             }
         }
-    }
-
-    public static void duplicate(int[] arr){
-        bs(arr);
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int i=0;i<arr.length - 1;i++){
-            if (arr[i] != arr[i+1]) {
-                res.add(arr[i]);
-            }
-        }
-        res.add(arr[arr.length - 1]);
-        System.out.println(res);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
@@ -64,7 +91,9 @@ public class bubblesort {
         // bubbleSort(arr);
         // int[] arr = {5, 2, 9, 1, 5, 6};
         // q1(arr);
-        int[] arr = {4, 5, 5, 2, 2, 1};
-        duplicate(arr);
+        // int[] arr = {4, 5, 5, 2, 2, 1};
+        // duplicate(arr);
+        String[] arr = {"apple", "pie", "banana", "kiwi"};
+        strLenArr(arr);
     }
 }
