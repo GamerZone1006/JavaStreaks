57317c20519416112931568638cede2475072304
GamerZone1006 <anushkayadav1006@gmail.com>
2025-05-30 14:50:00 +0530

    Recursion

--- recursion2.java
+++ recursion2.java
@@ -18,15 +18,44 @@ public class recursion2 {
     // }
 
     //sum of n numbers
-    public static int sumOfnNos(int n){
-        if (n<=1) {
-            return n;
+    // public static int sumOfnNos(int n){
+    //     if (n<=1) {
+    //         return n;
+    //     }
+    //     return n + sumOfnNos(n-1);
+    // }
+
+    //Sum of digits
+    // public static int sumOfDigits(int n){
+    //     if (n==0) {
+    //         return 0;
+    //     }
+    //     return (n%10) + sumOfDigits(n/10);
+    // }
+
+    // product of digits
+    // public static int product(int n){
+    //     if (n<=1) {
+    //         return 1;
+    //     }
+    //     return n%10 * product(n/10);
+    // }
+
+    //Reverse a number using recursion
+    public static void revNo(int n){
+        if(n<1){
+            return;
         }
-        return n + sumOfnNos(n-1);
+        System.out.print((n%10));
+        revNo(n/10);
     }
     public static void main(String[] args) {
         // fun(5);
         // System.out.println(factorial(5));
-        System.out.println(sumOfnNos(5));
+        // System.out.println(sumOfnNos(5));
+        // System.out.println(sumOfDigits(1546));
+        // System.out.println(product(1234));
+        // System.out.println(product(505));
+        revNo(1234);
     }
 }
