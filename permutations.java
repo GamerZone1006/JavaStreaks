08d61c704e8ef6488262274fdc7cd8a431f6bf36
GamerZone1006 <anushkayadav1006@gmail.com>
2025-06-05 14:37:29 +0530

    Permutations

--- permutations.java
+++ permutations.java
@@ -17,24 +17,40 @@ public class permutations {
     // }
 
     //returning arraylist of permutation of string
-    public static ArrayList<String> per(String p, String up){
+    // public static ArrayList<String> per(String p, String up){
+    //     if (up.isEmpty()) {
+    //         ArrayList<String> res = new ArrayList<>();
+    //         res.add(p);
+    //         return res;
+    //     }
+    //     char ch = up.charAt(0);
+    //     ArrayList<String> list = new ArrayList<>();
+    //     for(int i=0;i<=p.length();i++){
+    //         String f = p.substring(0, i);
+    //         String e = p.substring(i, p.length());
+    //         list.addAll(per(f+ch+e, up.substring(1)));
+    //     }
+    //     return list;
+    // }
+
+    //counting number of permutations
+    public static int noofper(String p, String up){
         if (up.isEmpty()) {
-            ArrayList<String> res = new ArrayList<>();
-            res.add(p);
-            return res;
+            return 1;
         }
+        int count = 0;
         char ch = up.charAt(0);
-        ArrayList<String> list = new ArrayList<>();
         for(int i=0;i<=p.length();i++){
             String f = p.substring(0, i);
             String e = p.substring(i, p.length());
-            list.addAll(per(f+ch+e, up.substring(1)));
+            count += noofper(f+ch+e, up.substring(1));
         }
-        return list;
+        return count;
     }
 
     public static void main(String[] args) {
         // per("", "abc");
-        System.out.println(per("", "abc"));
+        // System.out.println(per("", "abc"));
+        System.out.println(noofper("", "abc"));
     }
 }
