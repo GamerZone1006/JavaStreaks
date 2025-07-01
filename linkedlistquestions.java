c3e08a9956026e6395d2e9f708adb8ec7b2482bb
GamerZone1006 <anushkayadav1006@gmail.com>
2025-07-01 23:53:45 +0530

    linkedlist

--- linkedlistquestions.java
+++ linkedlistquestions.java
@@ -161,27 +161,27 @@ class LLQuestions{
     //     }
     // }
 
-    public static LinkNode getIntersectionNode(ListNode headA, ListNode headB){
-        if (headA == null || headB == null) {
-            return null;
-        }
-        ListNode a = headA;
-        ListNode b = headB;
-        while (a != b) {
-            if (a == null) {
-                a = headB;
-            }
-            else{
-                a = a.next;
-            }
-            if (b == null) {
-                b = b.headA;
-            }
-            else{
-                b = b.next;
-            }
-        }
-    }
+    // public static LinkNode getIntersectionNode(ListNode headA, ListNode headB){
+    //     if (headA == null || headB == null) {
+    //         return null;
+    //     }
+    //     ListNode a = headA;
+    //     ListNode b = headB;
+    //     while (a != b) {
+    //         if (a == null) {
+    //             a = headB;
+    //         }
+    //         else{
+    //             a = a.next;
+    //         }
+    //         if (b == null) {
+    //             b = b.headA;
+    //         }
+    //         else{
+    //             b = b.next;
+    //         }
+    //     }
+    // }
 }
 
 // import java.util.LinkedList;
