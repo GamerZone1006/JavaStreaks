3a9a4fef141667d112aebfbd736d5b87f357a56c
GamerZone1006 <anushkayadav1006@gmail.com>
2025-07-06 21:48:17 +0530

    binary tree

--- binarytree.java
+++ binarytree.java
@@ -57,57 +57,106 @@
 
 import java.util.Scanner;
 
+// class Node{
+//     int value;
+//     Node left;
+//     Node right;
+//     public Node(int value){
+//         this.value = value;
+//     }
+// }
+// public class binarytree {
+//     Node root;
+//     public void populate(Scanner sc){
+//         System.out.println("Enter root node value: ");
+//         int value = sc.nextInt();
+//         root = new Node(value);
+//         populateHelper(sc, root);
+//     }
+//     public void populateHelper(Scanner sc, Node node){
+//         System.out.print("Do u want to add left node: ");
+//         boolean left = sc.nextBoolean();
+//         if (left) {
+//             System.out.print("Enter value of left node: ");
+//             int value = sc.nextInt();
+//             node.left = new Node(value);
+//             populateHelper(sc, node.left);
+//         }
+
+//         System.out.print("Do u wanna add right node: ");
+//         boolean right = sc.nextBoolean();
+//         if (right) {
+//             System.out.print("Enter value of right node: ");
+//             int value = sc.nextInt();
+//             node.right = new Node(value);
+//             populateHelper(sc, node.right);
+//         }
+//     }
+//     void preorder(Node node){
+//         if (node == null) {
+//             return;
+//         }
+//         System.out.print(node.value+" ");
+//         preorder(node.left);
+//         preorder(node.right);
+//     }
+//     void postorder(Node node){
+//         if (node == null) {
+//             return;
+//         }
+//         postorder(node.left);
+//         postorder(node.right);
+//         System.out.print(node.value+" ");
+//     }
+//     void inorder(Node node){
+//         if (node == null) {
+//             return;
+//         }
+//         inorder(node.left);
+//         System.out.print(node.value+" ");
+//         inorder(node.right);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         binarytree tree = new binarytree();
+//         tree.populate(sc);
+//         System.out.print("\nInorder traversal: ");
+//         tree.inorder(tree.root);
+//         System.out.print("\nPreorder traversal: ");
+//         tree.preorder(tree.root);
+//         System.out.print("\nPostorder traversal: ");
+//         tree.postorder(tree.root);
+//         System.out.println();
+//         sc.close();
+//     }
+// }
+
+//Binary Search Tree
 class Node{
     int value;
-    Node left;
-    Node right;
+    Node left, right;
     public Node(int value){
         this.value = value;
     }
 }
 public class binarytree {
     Node root;
-    public void populate(Scanner sc){
-        System.out.println("Enter root node value: ");
-        int value = sc.nextInt();
-        root = new Node(value);
-        populateHelper(sc, root);
+    public void insert(int value){
+        root = insertRecursive(root, value);
     }
-    public void populateHelper(Scanner sc, Node node){
-        System.out.print("Do u want to add left node: ");
-        boolean left = sc.nextBoolean();
-        if (left) {
-            System.out.print("Enter value of left node: ");
-            int value = sc.nextInt();
-            node.left = new Node(value);
-            populateHelper(sc, node.left);
+    private Node insertRecursive(Node node, int value){
+        if (node == null) {
+            return new Node(value);
         }
+        if(value<node.value){
+            node.left = insertRecursive(node.left, value);
+        }
+        else{
+            node.right = insertRecursive(node.right, value);
+        }
+        return node;
+    }
 
-        System.out.print("Do u wanna add right node: ");
-        boolean right = sc.nextBoolean();
-        if (right) {
-            System.out.print("Enter value of right node: ");
-            int value = sc.nextInt();
-            node.right = new Node(value);
-            populateHelper(sc, node.right);
-        }
-    }
-    void preorder(Node node){
-        if (node == null) {
-            return;
-        }
-        System.out.print(node.value+" ");
-        preorder(node.left);
-        preorder(node.right);
-    }
-    void postorder(Node node){
-        if (node == null) {
-            return;
-        }
-        postorder(node.left);
-        postorder(node.right);
-        System.out.print(node.value+" ");
-    }
     void inorder(Node node){
         if (node == null) {
             return;
@@ -116,17 +165,20 @@ public class binarytree {
         System.out.print(node.value+" ");
         inorder(node.right);
     }
+    void preorder(Node node){
+        if(node == null) return;
+        System.out.print(node.value + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+    void postorder(Node node){
+        if(node == null) return;
+        postorder(node.left);
+        postorder(node.right);
+        System.out.println(node.value + " ");
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        binarytree tree = new binarytree();
-        tree.populate(sc);
-        System.out.print("\nInorder traversal: ");
-        tree.inorder(tree.root);
-        System.out.print("\nPreorder traversal: ");
-        tree.preorder(tree.root);
-        System.out.print("\nPostorder traversal: ");
-        tree.postorder(tree.root);
-        System.out.println();
-        sc.close();
+        
     }
 }
