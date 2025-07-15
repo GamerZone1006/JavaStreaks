81a33f6cf8d7b570f765bcdd76067f53e243f3df
GamerZone1006 <anushkayadav1006@gmail.com>
2025-07-15 23:02:11 +0530

    binary search

--- binarysearch.java
+++ binarysearch.java
@@ -1,23 +1,22 @@
-import java.util.ArrayList;
 
 public class binarysearch {
-    public static int bs(int[] arr, int target, int start, int end){
-        start = 0;
-        end = arr.length - 1;
-        while(start<=end){
-            int mid = (end - start)/2 + start;
-            if (arr[mid] == target) {
-                return mid;
-            }
-            else if (target< arr[mid]) {
-                end = mid - 1;
-            }
-            else{
-                start = mid + 1;
-            }
-        }
-        return -1;
-    }
+    // public static int bs(int[] arr, int target, int start, int end){
+    //     start = 0;
+    //     end = arr.length - 1;
+    //     while(start<=end){
+    //         int mid = (end - start)/2 + start;
+    //         if (arr[mid] == target) {
+    //             return mid;
+    //         }
+    //         else if (target< arr[mid]) {
+    //             end = mid - 1;
+    //         }
+    //         else{
+    //             start = mid + 1;
+    //         }
+    //     }
+    //     return -1;
+    // }
 
     // public static boolean isAesc(int[] arr){
     //     int start = 0;
@@ -122,15 +121,92 @@ public class binarysearch {
     //     return res;
     // }
 
-    public static int infArray(int[] arr, int target){
-        int start = 0;
-        int end = 1;
-        while (target<arr[end]) {
-            int newStart = end + 1;
-            end = end + (end - start + 1)*2;
-            start = newStart;
-        }
-        return bs(arr, target, start, end);
+    // public static int infArray(int[] arr, int target){
+    //     int start = 0;
+    //     int end = 1;
+    //     while (target<arr[end]) {
+    //         int newStart = end + 1;
+    //         end = end + (end - start + 1)*2;
+    //         start = newStart;
+    //     }
+    //     return bs(arr, target, start, end);
+    // }
+
+    // public static int binarySearch(int[] arr, int target, int start, int end){
+    //     while(start<=end){
+    //         int mid = start + (end - start)/2;
+    //         if(target == arr[mid]){
+    //             return mid;
+    //         }
+    //         else if(target<arr[mid]){
+    //             end = mid - 1;
+    //         }
+    //         else{
+    //             start = mid+1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+ 
+    // public static int findRange(int[] arr, int target){
+    //     int start = 0;
+    //     int end = 1;
+    //     while(end<arr.length && arr[end] < target){
+    //         start = end;
+    //         end = 2*end;
+    //         if(end >= arr.length){
+    //             end = arr.length - 1;
+    //             break;
+    //         }
+    //     }
+    //     return binarySearch(arr, target, start, end);
+    // }
+
+    // public int findInMountainArray(int target, MountainArray mountainArr) {
+    //     int n = mountainArr.length();
+    //     int start = 0;
+    //     int end = n-1;
+    //     int mid = -1;
+    //     while(start<=end){
+    //         mid = start + (end - start)/2;
+    //         int current = mountainArr.get(mid);
+    //         if(mid != 0 && mid != n-1 && current>mountainArr.get(mid-1)  && current > mountainArr.get(mid+1)){
+    //             return mid;
+    //         }
+    //         break;
+    //         else if(mid != n-1 && current < mountainArr.get(mid+1)){
+    //             start = mid+1;
+    //         }
+    //         else{
+    //             end = mid -1;
+    //         }
+    //     }
+    //     start = 0;
+    //     end = mid - 1;
+    //     while(start<=end){
+    //         int m = start + (end - start)/2;
+    //         int val = mountainArr.get(m);
+    //         if(val == target){
+    //             return m;
+    //         }
+    //         else if(target<val){
+    //             end = m-1;
+    //         }
+    //         else{
+    //             start = m+1;
+    //         }
+    //     }  
+    // }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, res, 0, nums1.length);
+        System.arraycopy(nums2, 0, res, nums1.length, res.length);
+        Arrays.sort(res);
+        int low = 0;
+        int high = res.length - 1;
+        double median = (low + high)/2;
+        return median;
     }
 
     public static void main(String[] args) {
@@ -149,8 +225,9 @@ public class binarysearch {
         // int[] arr = {1, 1, 1, 2, 2, 2, 2, 2};
         // int target = 2;
         // ArrayList<Integer> res = occur(arr, target);
-        int target = 55;
-        int res = infArray(arr, target);
-        System.out.println(res);
+        // int target = 55;
+        // int res = infArray(arr, target);
+        // System.out.println(res);
+        // System.out.println(findRange(arr, 20));
     }
 }
