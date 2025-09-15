12efe07ce7c741618f467a5cf7b5af80c785299f
GamerZone1006 <anushkayadav1006@gmail.com>
2025-09-15 21:50:58 +0530

    sheet solving

--- startserev.java
+++ startserev.java
@@ -102,11 +102,19 @@ public class startserev{
     //     return list;
     // }
 
-    public static boolean isPrime(int n){
-        for(int i=2;i<n;i++){
-            if(n%i == 0) return false;
+    // public static boolean isPrime(int n){
+    //     for(int i=2;i<n;i++){
+    //         if(n%i == 0) return false;
+    //     }
+    //     return true;
+    // }
+
+    public static void oneton(int n){
+        if(n==0){
+            return;
         }
-        return true;
+        oneton(n-1);
+        System.out.println(n);
     }
 
     public static void main(String[] args) {
@@ -132,7 +140,8 @@ public class startserev{
         // ArrayList<Integer> list = new ArrayList<>();
         // list = divisors(36);
         // System.out.println(list);
-        System.out.println(isPrime(14));
+        // System.out.println(isPrime(14));
+        oneton(5);
         // sc.close();
     }
 }
